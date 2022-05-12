6fa40a32125e66a4b01cf118bd5371f99d37a7d4
Maxi Alzugaray <maxialz99@hotmail.com>
2022-05-12 15:12:30 -0300

    boton de login en navbar

--- scripts/autenticacion.js
+++ scripts/autenticacion.js
@@ -2,4 +2,8 @@ const usuarioLogueado = localStorage.getItem("logueado")
 if (!usuarioLogueado) {
     alert("No tiene permisos,debe ingresar a su cuenta")
     window.location.href = "/index.html"
+}else if(usuarioLogueado){
+    document.getElementById(`BotonLogin`).className="d-none"
+    document.getElementById(`botonAdmin`).className="nav-item dropdown d-flex justify-content-start"
 }
+
